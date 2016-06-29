3db344523e44926b658af8aef3f13c17777aea0b
Leonardo Pizarro <lepizarr@inf.utfsm.cl>
2016-06-29 18:36:34 -0400

    adding bind in pub

--- pub.py
+++ pub.py
@@ -5,7 +5,7 @@ import zmq
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.setsockopt(zmq.LINGER, 0)    # discard unsent messages on close
-socket.connect('epgm://239.192.1.1:5000')
+socket.bind('epgm://239.192.1.1:5000')
 
 while True:
     msg = raw_input('> ')
