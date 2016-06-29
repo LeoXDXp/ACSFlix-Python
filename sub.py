82425eb4b23d347fda480996cfb5aa2e5486d3c6
Leonardo Pizarro <lepizarr@inf.utfsm.cl>
2016-06-29 18:49:21 -0400

    testing subscription to * channel

--- sub.py
+++ sub.py
@@ -5,7 +5,7 @@ import zmq
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 socket.connect('pgm://239.192.1.1:5000')
-socket.setsockopt(zmq.SUBSCRIBE, '+')
+socket.setsockopt(zmq.SUBSCRIBE, "")
 
 while True:
     data = socket.recv()
