b330244f1fc274f1f36cd1a337141fc62c55f958
Leonardo Pizarro <lepizarr@inf.utfsm.cl>
2016-07-01 14:42:45 -0400

    changing small size

--- pub.py
+++ pub.py
@@ -7,12 +7,12 @@ socket = context.socket(zmq.PUB)
 socket.setsockopt(zmq.LINGER, 0)    # discard unsent messages on close
 socket.bind('pgm://239.192.1.1:5000')
 
-block_size = 1024
+block_size = 256
 video_file = "/root/video4k/Sony_4K_Camp.mp4"
 
 try:
     video = open(video_file,"rb")
-    block_total = ( os.path.getsize(video_file) / 1024 ) + 1
+    block_total = ( os.path.getsize(video_file) / block_size ) + 1
     block_num = 1
 
     while block_total > block_num:
